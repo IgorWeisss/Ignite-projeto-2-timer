ac23f3fbc78dc083b3c99d5aff0820aa7306e98f
Igor - Ubuntu <igorweiss@hotmail.com.br>
2023-05-01 21:55:20 -0300

    React-router-DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
--- src/components/Button.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonContainer = styled.button`
-  width: 100px;
-  height: 100px;
-  border-radius: 8px;
-  margin-right: 1rem;
-  background-color: ${(props) => props.theme['green-500']};
-`
